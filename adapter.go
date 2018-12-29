18434119d46f9b576b6fe2855de11be45a247f63
root <root@cpe-172-72-33-11.carolina.res.rr.com>
2018-12-29 09:48:18 +0800

    skip empty field values to be conditions of deletion

--- adapter.go
+++ adapter.go
@@ -280,27 +280,39 @@ func (a *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int,
 	filter = append(filter, "p_type")
 	if fieldIndex <= 0 && 0 < fieldIndex + len(fieldValues) {
 		line.V0 = fieldValues[0 - fieldIndex]
-		filter = append(filter, "v0")
+		if line.V0 != "" {
+			filter = append(filter, "v0")
+		}
 	}
 	if fieldIndex <= 1 && 1 < fieldIndex + len(fieldValues) {
 		line.V1 = fieldValues[1 - fieldIndex]
-		filter = append(filter, "v1")
+		if line.V1 != "" {
+			filter = append(filter, "v1")
+		}
 	}
 	if fieldIndex <= 2 && 2 < fieldIndex + len(fieldValues) {
 		line.V2 = fieldValues[2 - fieldIndex]
-		filter = append(filter, "v2")
+		if line.V2 != "" {
+			filter = append(filter, "v2")
+		}
 	}
 	if fieldIndex <= 3 && 3 < fieldIndex + len(fieldValues) {
 		line.V3 = fieldValues[3 - fieldIndex]
-		filter = append(filter, "v3")
+		if line.V3 != "" {
+			filter = append(filter, "v3")
+		}
 	}
 	if fieldIndex <= 4 && 4 < fieldIndex + len(fieldValues) {
 		line.V4 = fieldValues[4 - fieldIndex]
-		filter = append(filter, "v4")
+		if line.V4 != "" {
+			filter = append(filter, "v4")
+		}
 	}
 	if fieldIndex <= 5 && 5 < fieldIndex + len(fieldValues) {
 		line.V5 = fieldValues[5 - fieldIndex]
-		filter = append(filter, "v5")
+		if line.V5 != "" {
+			filter = append(filter, "v5")
+		}
 	}
 
 	_, err := a.o.Delete(&line, filter...)
